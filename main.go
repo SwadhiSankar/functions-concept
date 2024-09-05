9eabb250670f851d52c255e696a4c83c9bd30502
SwadhiSankar <swadhis14@gmail.com>
2024-09-05 17:02:50 +0530

    passing function as args to a method and make use of it

--- main.go
+++ main.go
@@ -4,18 +4,28 @@ import "fmt"
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
-	doubled := doubleNumber(&numbers)
+	doubled := transformNumbers(&numbers, double) // no parenthesis for double -> to passing func and not returning the result of the func if we give () it will return the result
+	triple := transformNumbers(&numbers, triple)
 
 	fmt.Println(doubled)
+	fmt.Println(triple)
 }
 
-func doubleNumber(numbers *[]int)[]int{
+func transformNumbers(numbers *[]int, tranform func(int)int)[]int{
 	dNumber := []int{}
 
 	for _, value := range *numbers{
-		dNumber = append(dNumber,value * 2)
+		dNumber = append(dNumber,tranform(value))
 	}
 
 	return dNumber
 
+	
+}
+func double(number int ) int{
+	return number * 2
+}
+
+func triple(number int) int{
+	return number * 3
 }
